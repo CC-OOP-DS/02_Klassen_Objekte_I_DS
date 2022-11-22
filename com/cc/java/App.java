250b1a321d75351941ba11245e6dbb2cc576ee2d
Daniel <schindlerdaniel@web.de>
2022-11-22 12:42:57 +0100

    class

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,11 +3,20 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+        new Cat();
+        Cat cat = new Cat();                // Jede Klasse ist ein Datentyp!
+        output("Blick von aussen: " + cat);
+        cat.tellYourAddress();
+
+        output("-------------------");
+
+        Cat cat1 = new Cat();                
+        output("Blick von aussen: " + cat1);
+        cat1.tellYourAddress();
     }
 
-    public static void output(String outputStr) {
-        System.out.println(outputStr);
+    public static void output(String string) {
+        System.out.println(string);
     }
 
 
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,8 @@
+package com.cc.java;
+
+public class Cat {
+    
+    public void tellYourAddress(){
+        App.output("Blick von innen: " + this);
+    }
+}
