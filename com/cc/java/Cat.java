19c42e9c7e2b27f8bb026b2b5cc770aa35948013
Daniel <schindlerdaniel@web.de>
2022-11-23 10:47:25 +0100

    Console

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,8 +1,35 @@
 package com.cc.java;
 
 public class Cat {
-    
-    public void tellYourAddress(){
-        App.output("Blick von innen: " + this);
-    }
+  
+    // Variablen immer mit Datentyp zuweisen!
+    public String name;   
+    public String furColor;
+    public int age;
+
+
+//Constructor erstellen
+public Cat(String name, String furColor, int age){
+    this.name = name;
+    this.furColor = furColor;
+    this.age = age;
+}
+
+
+public String tellYourName() {
+    return this.name;
+}
+  
+public String tellYourColor(){
+    return this.furColor;
+}
+
+public int tellYourAge(){
+    return this.age;
+}
+
+public void tellYourAddress(){
+    App.output("Blick von innen: " + this);  //this = Instanzvariabel, Referenz des Objektes auf sich selbst.
+}
+
 }
